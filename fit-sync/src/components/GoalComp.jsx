720992cab32b02648e26bf339e76271db2d84663
AyGeeEm <aagarciamarin@gmail.com>
2024-11-29 21:47:45 -0500

    added goal model to profile goal component

--- fit-sync/src/components/GoalComp.jsx
+++ fit-sync/src/components/GoalComp.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './GoalComp.css';
 
-const GoalComp = ({ name = 'N/A', description = 'N/A', targetDate = 'N/A' }) => {
+const GoalComp = ({ title, description, dueDate }) => {
+    const formatDate = (date) => {
+        return new Date(date).toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+    };
+
     return (
-        <div className="goal-comp">
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <p><strong>Target Date:</strong> {targetDate}</p>
+        <div className = "goal-comp">
+            <h2>{title}</h2>
+            <p><em>{description}</em></p>
+            <p><strong>Target Date:</strong> {formatDate(dueDate)}</p>
         </div>
     );
 };
 
 GoalComp.propTypes = {
-    name: PropTypes.string,
-    description: PropTypes.string,
-    targetDate: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    dueDate: PropTypes.string.isRequired,
+};
+
+GoalComp.defaultProps = {
+    title: 'No Title',
+    description: 'No Description',
+    dueDate: new Date().toISOString()
 };
 
 export default GoalComp;
